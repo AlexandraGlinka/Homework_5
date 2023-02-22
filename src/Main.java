2f77f9b0ccb89681bdc4aaabae3e55248e68b846
glinka <alexandraglinka@yandex.ru>
2023-02-22 21:47:14 +0300

    Task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        //task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        char clientOS = 0; //0 — iOS, 1 — Android
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
