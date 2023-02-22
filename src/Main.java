f0787a574ca2754ff4e26391dc2f1588ffb62343
glinka <alexandraglinka@yandex.ru>
2023-02-22 21:58:44 +0300

    Task_2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
     }
 
     public static void task1() {
@@ -11,6 +11,24 @@ public class Main {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 1");
+        char clientOS = 1; //0 — iOS, 1 — Android
+        int clientDeviceYear = 2014;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
         }
     }
 }
