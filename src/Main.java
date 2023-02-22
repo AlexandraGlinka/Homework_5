0acc617fa149218a3baee63e08c4b9901bdd2a8e
glinka <alexandraglinka@yandex.ru>
2023-02-22 22:27:20 +0300

    Task_4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
 
     public static void task1() {
@@ -30,5 +32,29 @@ public class Main {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2500;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println("Год является високосным");
+            } else {
+            System.out.println("Год не является високосным");
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 59;
+        int deliveryTime = 1;
+        if (deliveryDistance < 20) {
+            //deliveryTime = deliveryTime;
+        } else if (deliveryDistance < 60) {
+            deliveryTime += 1;
+        } else if (deliveryDistance < 100) {
+            deliveryTime += 2;
+        } else {
+            System.out.println("Свыше 100 км доставки нет");
+        }
+        System.out.println("Потребуется дней: " + deliveryTime);
     }
 }
